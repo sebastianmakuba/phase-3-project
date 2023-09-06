6d5063f8a5d207d442037367513dd827dac4d0cc
+ <+>
2023-09-06 10:28:36 +0300

    updated database.py

--- profiles/database.py
+++ profiles/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-DATABASE_URL = 'sqlite:///library.db'  # Change this to your preferred database URL
+DATABASE_URL = 'sqlite:///library.db' 
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
